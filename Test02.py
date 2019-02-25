a12f7cfa79c132947e982ad7fbb6d5a52846f0a6
Woranat Swangswai <42377055+Sleepingsong@users.noreply.github.com>
2019-02-25 13:07:22 +0700

    - Show Invoice data

--- Test02.py
+++ Test02.py
@@ -13,33 +13,44 @@ tax_enter = Entry(root)
 tax_enter.grid(row = 0)
 button1 = Button(root, text = 'ค้นหา', command= lambda: tax_search())
 button1.grid(row = 1)
+tree = ttk.Treeview(root, height = 20, column = ("1"))
+tree.heading('#0', text = "ประเภท")
+tree.heading(0, text = "ผลลัพธ์")
+tree.grid(row = 2)
 
 
 def tax_search():
+    try:
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        session = Session()
+        session.verify = False
+        transport = Transport(session=session)
 
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    session = Session()
-    session.verify = False
-    transport = Transport(session=session)
+        client = Client('https://rdws.rd.go.th/serviceRD3/vatserviceRD3.asmx?wsdl',
+                        transport=transport)
+        result = client.service.Service(
+            username='anonymous',
+            password='anonymous',
+            TIN = tax_enter.get(),
+            ProvinceCode=0,
+            BranchNumber=0,
+            AmphurCode=0
+        )
 
-    client = Client('https://rdws.rd.go.th/serviceRD3/vatserviceRD3.asmx?wsdl',
-                    transport=transport)
-    result = client.service.Service(
-        username='anonymous',
-        password='anonymous',
-        TIN = tax_enter.get(),
-        ProvinceCode=0,
-        BranchNumber=0,
-        AmphurCode=0
-    )
-    # Convert Zeep Response object (in this case Service) to Python dict.
-    result = zeep.helpers.serialize_object(result)
-    # print(result)
-    for k in result.keys():
-        # print(k, result[k])
-        if result[k] is not None:
-            v = result[k].get('anyType', None)[0]
-            print(k,v)
+        # Convert Zeep Response object (in this case Service) to Python dict.
+        result = zeep.helpers.serialize_object(result)
+
+        # print(result)
+        for k in result.keys():
+            if result[k] is not None:
+                v = result[k].get('anyType', None)[0]
+                tree.insert('', 'end', text=k, value = v)
+
+    except:
+        message1 = Label(root,text = "โปรดลองอีกครั้ง")
+        message1.grid(row = 1 , column =1)
+
+
 
 
 root.geometry('800x600')
