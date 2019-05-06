5ed5b1f3ee2c010f5d435b1cd19d80910695c6b1
Woranat Swangswai <Sleepingsong@users.noreply.github.com>
2019-05-06 19:10:45 +0700

    - Print reciept

--- Test.py
+++ Test.py
@@ -1,11 +1,1 @@
-from pymongo import MongoClient
 
-
-
-client = MongoClient("mongodb+srv://AtAdmin:admin@seniorproject-yv211.gcp.mongodb.net/test?retryWrites=true")
-
-db = client['MyDatabase']
-mycol = db['Customer']
-
-mydoc = mycol.find_one()
-print(mydoc)
